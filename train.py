5dcb0a20ad8fca7e9254c643dbd2c6c04040e1df
poopaandloopa <andr.krasnyh@gmail.com>
2024-02-09 00:46:55 +0200

    Initial train script logic

---
+++ train.py
@@ -0,0 +1,45 @@
+from ultralytics import YOLO
+from argparse import ArgumentParser
+from utils import download_rsna_dataset, create_annotation_folder_from_csv, convert_dicom_to_jpeg, train_val_test_split
+
+
+DEFAULT_CSV_ANNOTATIONS_PATH = "data/RSNA_data/stage_2_train_labels.csv"
+DEFAULT_YOLO_ANNOTATIONS_PATH = "data/labels/"
+DEFAULT_TRAIN_IMAGES_PATH = "data/RSNA_data/train_dicom/"
+DEFAULT_TEST_IMAGES_PATH = "data/RSNA_data/test_dicom/"
+DEFAULT_JPEG_IMAGES_PATH = "data/images/"
+
+
+def main():
+    parser = ArgumentParser(description="Train YOLOv8")
+    # data options
+    parser.add_argument('--download_dataset', action="store_true", default=False)
+    parser.add_argument('--train_images', type=str, default=DEFAULT_TRAIN_IMAGES_PATH)
+    parser.add_argument('--csv_annotations', type=str, default=DEFAULT_CSV_ANNOTATIONS_PATH)
+
+    # train options
+    # parser.add_argument()
+    args = parser.parse_args()
+
+    # download RSNA dataset
+    if args.download_dataset:
+        download_rsna_dataset()
+
+    # convert annotations to YOLOv8 format
+    print("Creating YOLOv8 annotations for training data...")
+    create_annotation_folder_from_csv(csv_annotation_path=args.csv_annotations,
+                                      annotation_folder_path=DEFAULT_YOLO_ANNOTATIONS_PATH)
+    # convert DICOM images to JPEG
+    print("Converting DICOM images to JPEG...")
+    convert_dicom_to_jpeg(dicom_folder_path=args.train_images,
+                          jpeg_folder_path=DEFAULT_JPEG_IMAGES_PATH)
+    # split train data into train, val and test
+    print("Splitting train data...")
+    train_val_test_split(images_folder_path=DEFAULT_JPEG_IMAGES_PATH,
+                         annotation_folder_path=DEFAULT_YOLO_ANNOTATIONS_PATH,
+                         val_frac=0.1)
+
+    # TODO: fit YOLOv8 with data.yaml file
+
+if __name__ == '__main__':
+    main()
